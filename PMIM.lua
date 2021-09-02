e6570f848ae14ad836fd697d6fabf7c16d3b0341
Brad Byun <bradbyun1@gmail.com>
2021-09-02 01:17:30 -0700

    4th commit. Scanning inventory to mark items as junk, as well as selling all junk items at a vendor are now functional.

--- PMIM.lua
+++ PMIM.lua
@@ -1,5 +1,4 @@
 PMIM = {}
---local PMIM = PMIM
 
 NAME = "PMIM"
 VERSION = 1
@@ -32,80 +31,94 @@ function PMIM.getVersion()
     return VERSION
 end
 
-function PMIM.markJunkType(counter, itemId)
-    if counter == 2 then
-        if itemId == 33265 then --33265 = Soul Gem (Empty)
-            SetItemIsJunk(itemId)
-        end
-    end
-end
-
-function PMIM.filterToJunk(itemId, bagType, slotIndex)
-    d("Step 1")
+function PMIM.filterToJunk(itemId, itemType, itemQuality, itemLevel, itemName, itemTrait, bagType, slotIndex)
+    --Checks if the setting is on, then checks if the item ID is the soul gem (empty) item ID [33265]
     if PMIM.preferences.toSell.sellEmptyGems then
-        d("Step 2")
         if itemId == 33265 then
-            d("Step 3")
             SetItemIsJunk(bagType, slotIndex, true)
+            return
+        end
+    end
+    --Checks if the setting is on, then checks if the item type is any sort of glyph
+    --Afterwards, checks if the item is magic (green) rarity or less
+    if PMIM.preferences.toSell.sellGlyphs then
+        if (itemType == ITEMTYPE_GLYPH_ARMOR or itemType == ITEMTYPE_GLYPH_JEWELRY or itemType == ITEMTYPE_GLYPH_WEAPON) then
+            if (itemQuality <= ITEM_FUNCTIONAL_QUALITY_MAGIC) then
+                SetItemIsJunk(bagType, slotIndex, true)
+            end
+            return
+        end
+    end
+    --Checks if the setting is on, then checks if the item type is a tool, and if the item quality is magic (green)
+    --Afterwards, checks if the item level is neither 50 nor nil, excluding max level repair kits
+    if PMIM.preferences.toSell.sellLowRepairs then
+        if (itemType == ITEMTYPE_TOOL and itemQuality == ITEM_FUNCTIONAL_QUALITY_MAGIC) then
+            if (itemLevel ~= 50 and itemLevel ~= nil) then
+                SetItemIsJunk(bagType, slotIndex, true)
+            end
+            return
+        end
+    end
+    --Checks if the setting is on, then checks if item type is a refined crafting material
+    --Afterwards, checks if the item names match any of the max level crafting materials
+    if PMIM.preferences.toSell.sellLowMats then
+        if (itemType == ITEMTYPE_BLACKSMITHING_MATERIAL or itemType == ITEMTYPE_CLOTHIER_MATERIAL or itemType == ITEMTYPE_WOODWORKING_MATERIAL or itemType == ITEMTYPE_JEWELRYCRAFTING_MATERIAL) then
+            if (itemName == "Sanded Ruby Ash^ns" or itemName == "platinum ounce" or itemName == "Rubedite Ingot" or itemName == "Rubedo Leather^ns" or itemName == "Ancestor Silk^ns") then
+                return
+            else
+                SetItemIsJunk(bagType, slotIndex, true)
+            end
+            return
+        end
+    end
+    --Checks if the setting is on, then checks if the item trait is ornate
+    if PMIM.preferences.toSell.sellOrnateGear then
+        if (itemTrait == ITEM_TRAIT_TYPE_WEAPON_ORNATE or itemTrait == ITEM_TRAIT_TYPE_ARMOR_ORNATE or itemTrait == ITEM_TRAIT_TYPE_JEWELRY_ORNATE) then
+            SetItemIsJunk(bagType, slotIndex, true)
+            return
+        end
+    end
+    --Checks if the setting is on, then checks if the item trait is intricate
+    if PMIM.preferences.toSell.sellIntricateGear then
+        if (itemTrait == ITEM_TRAIT_TYPE_WEAPON_INTRICATE or itemTrait == ITEM_TRAIT_TYPE_ARMOR_INTRICATE or itemTrait == ITEM_TRAIT_TYPE_JEWELRY_INTRICATE) then
+            SetItemIsJunk(bagType, slotIndex, true)
+            return
         end
     end
 end
 
+--Iterates through the backpack inventory (1), then running the filter command to mark certain items as junk
 function PMIM.scanInventory(bagType)
     for slotIndex = 1, GetBagSize(1) do
         local itemId = GetItemId(bagType, slotIndex)
-        d(slotIndex .. ": " .. itemId)
+        local itemType = GetItemType(bagType, slotIndex)
+        local itemQuality = GetItemFunctionalQuality(bagType, slotIndex)
+        local itemLevel = GetItemLevel(bagType, slotIndex)
+        local itemLink = GetItemLink(bagType, slotIndex)
+        local itemName = GetItemLinkName(itemLink)
+        local itemTrait = GetItemTrait(bagType, slotIndex)
         if itemId ~= 0 and CanItemBeMarkedAsJunk(bagType, slotIndex) then
-            if itemId == 33265 then
-                d("Empty Soul Gems found! Filtering...")
-            end
-            PMIM.filterToJunk(itemId, bagType, slotIndex)
+            PMIM.filterToJunk(itemId, itemType, itemQuality, itemLevel, itemName, itemTrait, bagType, slotIndex)
         end
     end
 end
 
-
+--Creates the pscan and psell slash commands, used for scanning the inventory to mark certain items as junk
+--as well as selling said junk when in the merchant scene
 function PMIM:Initialize()
     self.preferences = ZO_SavedVars:NewAccountWide("PMIMSavedVariables", 1, nil, PMIM.defaultSettings)
-    SLASH_COMMANDS["/foobar"] = PMIM.test1
-    SLASH_COMMANDS["/foobar2"] = PMIM.sizeGetter
-    SLASH_COMMANDS["/foobar3"] = function() PMIM.scanInventory(1) end
-    SLASH_COMMANDS["/foobar4"] = PMIM.test2
-    SLASH_COMMANDS["/foobar4"] = PMIM.tableBools
+    SLASH_COMMANDS["/pscan"] = function() PMIM.scanInventory(1) end
+    SLASH_COMMANDS["/psell"] = function() PMIM.sellJunk(1) end
     PMIM.InitSettings()
-    --SLASH_COMMANDS["/scanjunk"] = PMIM.test1
-    --SLASH_COMMANDS["/selljunk"] = PMIM.sellJunk
-
-    --d("Testing123")
 end
 
-function PMIM.tableBools()
-    for sellType, bool in pairs(PMIM.preferences.toSell) do
-        local testVar = 0
-        if bool then
-            testVar = 1
+function PMIM.sellJunk(bagType)
+    for slotIndex = 1, GetBagSize(bagType) do
+        if IsItemJunk(bagType, slotIndex) then
+            local itemStack, _ = GetSlotStackSize(bagType, slotIndex)
+            SellInventoryItem(bagType, slotIndex, itemStack)
         end
-        d(sellType .. ": " .. testVar)
     end
-end
-
-function PMIM.test2()
-    local itemId = GetItemId(1, 30)
-    d("Empty Soul Gem is: " .. itemId)
-end
-
-function PMIM.sizeGetter()
-    d("Getting size...")
-    local bagSize = GetBagSize(1)
-    d("Your bag size is: " .. bagSize)
-end
-
-function PMIM.test1()
-    d("Hello World!")
-end
-
-function PMIM.sellJunk()
-    sellAllJunk()
 end
 
 function PMIM.OnAddonLoaded(event, addonName)
@@ -114,4 +127,5 @@ function PMIM.OnAddonLoaded(event, addonName)
     end
 end
 
+--Triggers when addon is loaded
 EVENT_MANAGER:RegisterForEvent(PMIM.name, EVENT_ADD_ON_LOADED, PMIM.OnAddonLoaded)
--- menu.lua
+++ menu.lua
@@ -1,5 +1,4 @@
 local LAM = LibAddonMenu2
---local PMIM = PoorMansInventoryManagement
 
 local panelData = {
     type = "panel",
@@ -9,6 +8,27 @@ local panelData = {
     version = "0.0.1",
     registerForRefresh = true,
 }
+
+--Settings here for easy reference
+--[[
+PMIM.defaultSettings = {
+    toSell = {
+        sellEmptyGems = false,
+        sellOrnateGear = false,
+        sellLowRepairs = false,
+        sellIntricateGear = false,
+        sellGlyphs = false,
+        sellLowMats = false,
+
+    },
+    toBank = {
+        bankHighMats = true,
+        bankHighRepairs = true,
+        bankTempers = true,
+        bankWrits = true,
+    },
+}
+]]
 
 local optionsTable = {
     {
@@ -29,13 +49,36 @@ local optionsTable = {
         setFunc = function(value) PMIM.preferences.toSell.sellGlyphs = value end,
         width = "half",
     },
+    {
+        type = "checkbox",
+        name = "Low Repair Kits",
+        getFunc = function() return PMIM.preferences.toSell.sellLowRepairs end,
+        setFunc = function(value) PMIM.preferences.toSell.sellLowRepairs = value end,
+        width = "half",
+    },
+    {
+        type = "checkbox",
+        name = "Low Refined Mats",
+        getFunc = function() return PMIM.preferences.toSell.sellLowMats end,
+        setFunc = function(value) PMIM.preferences.toSell.sellLowMats = value end,
+        width = "half",
+    },
+    {
+        type = "checkbox",
+        name = "Ornate Gear",
+        getFunc = function() return PMIM.preferences.toSell.sellOrnateGear end,
+        setFunc = function(value) PMIM.preferences.toSell.sellOrnateGear = value end,
+        width = "half",
+    },
+    {
+        type = "checkbox",
+        name = "Intricate Gear",
+        getFunc = function() return PMIM.preferences.toSell.sellIntricateGear end,
+        setFunc = function(value) PMIM.preferences.toSell.sellIntricateGear = value end,
+        width = "half",
+    },
 
 }
-
-
---function PMIM.GlyphJunk()
---    PMIM.defaultSettings.toSell.sellGlyphs = not PMIM.defaultSettings.toSell.sellGlyphs
---end
 
 function PMIM:InitSettings()
     name = PMIM.getName() .. "Menu"
